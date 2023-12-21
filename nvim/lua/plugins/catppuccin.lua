dbbe5d0c3c061725ae30fd81fe6f18579984d821
Khang Tran <khang.tm-ttv@tci-pf.com>
2023-12-21 22:11:54 +0700

    init branch

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+return {
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
+	-- lazy = false,
+	-- priority = 1000,
+	-- flavour = "mocha",
+	-- config = function()
+	-- 	vim.cmd("colorscheme catppuccin")
+	-- end,
+}
