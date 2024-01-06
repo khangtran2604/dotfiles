9fb8a06fd68d247c82b72ba530911afce129d300
Khang Tran <khangtran260495@gmail.com>
2024-01-06 13:03:21 +0700

    adding settings for Rust + tmux config

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -34,6 +34,7 @@ local plugins = {
         "prettier",
         "stylua",
         "typescript-language-server",
+        "rust-analyzer",
       },
     },
   },
@@ -160,6 +161,86 @@ local plugins = {
       require "custom.configs.symbols-outline"
     end,
   },
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      require("core.utils").load_mappings "dap"
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end,
+    init = function()
+      vim.g.dap_virtual_text = true
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  -- Rust Plugins
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "toml" },
+    config = function(_, opts)
+      local crates = require "crates"
+      crates.setup(opts)
+      require("cmp").setup.buffer {
+        sources = { { name = "crates" } },
+      }
+      crates.show()
+      require("core.utils").load_mappings "crates"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      -- M.completion.completeopt = "menu,menuone,noselect"
+      -- M.mapping["<CR>"] = cmp.mapping.confirm {
+      --   behavior = cmp.ConfirmBehavior.Insert,
+      --   select = false,
+      -- }
+      table.insert(M.sources, { name = "crates" })
+      return M
+    end,
+  },
+  -- End Rust Plugins
 }
 
 return plugins
