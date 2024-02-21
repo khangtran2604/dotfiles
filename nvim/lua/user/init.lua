462584986960ebd0fa835ff98f31c74870016836
Khang Tran <khangtran260495@gmail.com>
2024-02-21 21:20:26 +0700

    Using custom catppuccin theme

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -21,7 +21,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "everforest",
+  colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
