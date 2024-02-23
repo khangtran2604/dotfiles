f5e8aa5822e4de17480797ce245d1ab5c99bc9eb
Khang Tran <khangtran260495@gmail.com>
2024-02-23 21:46:21 +0700

    Init branch

---
+++ nvim/lua/custom/plugins/formatter.lua
@@ -0,0 +1,36 @@
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local conform = require 'conform'
+
+    conform.setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Use a sub-list to run only the first available formatter
+        html = { { 'prettierd', 'prettier' } },
+        css = { { 'prettierd', 'prettier' } },
+        json = { { 'prettierd', 'prettier' } },
+        yaml = { { 'prettierd', 'prettier' } },
+        markdown = { { 'prettierd', 'prettier' } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        javascriptreact = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        -- async = false,
+        timeout_ms = 500,
+      },
+    }
+
+    vim.keymap.set({ 'n', 'v' }, '<leader>fm', function()
+      conform.format {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      }
+    end, { desc = 'Format Document' })
+  end,
+}
