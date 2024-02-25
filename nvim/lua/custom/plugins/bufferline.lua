38c60b0f5236c0ff61e6360cb4e6567cbd8243bb
Khang Tran <khangtran260495@gmail.com>
2024-02-25 10:42:37 +0700

    Update bufferline plugin version

--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 return {
   'akinsho/bufferline.nvim',
-  version = 'v3.*',
+  version = '*',
   dependencies = { 'nvim-tree/nvim-web-devicons', 'famiu/bufdelete.nvim' },
   opts = {
     options = {
