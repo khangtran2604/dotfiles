db8584de8a7193add98d9c7fc35462260ebca5a8
Khang Tran <khang.tm-ttv@tci-pf.com>
2024-04-15 13:27:37 +0700

    Adding some experimental features

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -60,4 +60,39 @@ if g.neovide then
   g.neovide_remember_window_size = true
 end
 
+-- NOTE: EXPEREMENTAL: This is a new feature and may not work as expected
+vim.o.smartindent = true -- make indenting smarter again
+vim.o.swapfile = false -- creates a swapfile
+vim.o.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.o.updatetime = 100 -- faster completion (4000ms default)
+vim.o.expandtab = true -- convert tabs to spaces
+vim.o.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.o.tabstop = 2 -- insert 2 spaces for a tab
+
+vim.cmd 'set whichwrap+=<,>,[,],h,l'
+vim.cmd [[set iskeyword+=-]]
+
+-- In summary, this configuration snippet sets up an autocommand that automatically checks if the current file has been changed by another program every time you switch into a buffer window in Neovim. If there are external changes, it alerts you, ensuring that you're always working with the most up-to-date version of the file
+vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  pattern = { '*' },
+  callback = function()
+    vim.cmd 'checktime'
+  end,
+})
+
+-- Overall, this autocommand automates interaction with snippets from luasnip based on cursor behavior, contributing to a smoother and more intuitive editing experience in Neovim.
+vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+  callback = function()
+    local status_ok, luasnip = pcall(require, 'luasnip')
+    if not status_ok then
+      return
+    end
+    if luasnip.expand_or_jumpable() then
+      -- ask maintainer for option to make this silent
+      -- luasnip.unlink_current()
+      vim.cmd [[silent! lua require("luasnip").unlink_current()]]
+    end
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
