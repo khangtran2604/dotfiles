fc208f8182f3dab45036d19081d62d28b8efc48a
Khang Tran <khangtran260495@gmail.com>
2024-04-20 15:11:24 +0700

    Fix formatting issue + Update package

--- nvim/lua/custom/plugins/formatter.lua
+++ nvim/lua/custom/plugins/formatter.lua
@@ -1,3 +1,48 @@
+-- return {
+--   'stevearc/conform.nvim',
+--   -- event = { 'BufReadPre', 'BufNewFile' },
+--   config = function()
+--     local conform = require 'conform'
+--
+--     conform.setup {
+--       formatters_by_ft = {
+--         lua = { 'stylua' },
+--         -- Use a sub-list to run only the first available formatter
+--         html = { { 'prettierd', 'prettier' } },
+--         css = { { 'prettierd', 'prettier' } },
+--         json = { { 'prettierd', 'prettier' } },
+--         yaml = { { 'prettierd', 'prettier' } },
+--         markdown = { { 'prettierd', 'prettier' } },
+--         javascript = { { 'prettierd', 'prettier' } },
+--         typescript = { { 'prettierd', 'prettier' } },
+--         javascriptreact = { { 'prettierd', 'prettier' } },
+--         typescriptreact = { { 'prettierd', 'prettier' } },
+--         go = { { 'gofumpt', 'goimports-reviser' } },
+--       },
+--       format_on_save = {
+--         lsp_fallback = true,
+--         -- async = false,
+--         timeout_ms = 500,
+--       },
+--     }
+--
+--     vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
+--       conform.format {
+--         lsp_fallback = true,
+--         async = false,
+--         timeout_ms = 1000,
+--       }
+--     end, { desc = 'Format Code' })
+--
+--     vim.api.nvim_create_autocmd('BufWritePre', {
+--       pattern = '*',
+--       callback = function(args)
+--         require('conform').format { bufnr = args.buf }
+--       end,
+--     })
+--   end,
+-- }
+--
 return {
   'stevearc/conform.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
@@ -6,23 +51,25 @@ return {
 
     conform.setup {
       formatters_by_ft = {
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        svelte = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
+        graphql = { 'prettier' },
+        liquid = { 'prettier' },
         lua = { 'stylua' },
-        -- Use a sub-list to run only the first available formatter
-        html = { { 'prettierd', 'prettier' } },
-        css = { { 'prettierd', 'prettier' } },
-        json = { { 'prettierd', 'prettier' } },
-        yaml = { { 'prettierd', 'prettier' } },
-        markdown = { { 'prettierd', 'prettier' } },
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-        javascriptreact = { { 'prettierd', 'prettier' } },
-        typescriptreact = { { 'prettierd', 'prettier' } },
-        go = { { 'gofumpt', 'goimports-reviser' } },
+        python = { 'isort', 'black' },
       },
       format_on_save = {
         lsp_fallback = true,
-        -- async = false,
-        timeout_ms = 500,
+        async = false,
+        timeout_ms = 1000,
       },
     }
 
@@ -32,6 +79,6 @@ return {
         async = false,
         timeout_ms = 1000,
       }
-    end, { desc = 'Format Code' })
+    end, { desc = 'Format file or range (in visual mode)' })
   end,
 }
