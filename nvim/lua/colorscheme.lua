7577eef2bb45081cb8d990e9d839f6cfc0ad56c7
Khang Tran <khang.tm-ttv@tci-pf.com>
2024-05-31 13:43:32 +0700

    Transparent background

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -11,17 +11,27 @@ return {
 				globalstatus = true,
 				-- disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
 			},
-			overrides = {
-				Normal = { bg = "None" },
-				ColorColumn = { bg = "None" },
-				SignColumn = { bg = "None" },
-				Folded = { bg = "None" },
-				FoldColumn = { bg = "None" },
-				CursorLine = { bg = "None" },
-				CursorColumn = { bg = "None" },
-				WhichKeyFloat = { bg = "None" },
-				VertSplit = { bg = "None" },
-			},
+			-- overrides = {
+			-- 	Normal = { bg = "None" },
+			-- 	ColorColumn = { bg = "None" },
+			-- 	SignColumn = { bg = "None" },
+			-- 	Folded = { bg = "None" },
+			-- 	FoldColumn = { bg = "None" },
+			-- 	CursorLine = { bg = "None" },
+			-- 	CursorColumn = { bg = "None" },
+			-- 	WhichKeyFloat = { bg = "None" },
+			-- 	VertSplit = { bg = "None" },
+			-- },
 		})
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+		vim.api.nvim_set_hl(0, "CursorColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
 	end,
 }
