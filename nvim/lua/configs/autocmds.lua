658378c31d9dc399e79490cefac71a3ae4ecf980
Khang Tran <khangtran260495@gmail.com>
2024-06-01 20:41:24 +0700

    adding some workaround auto cmd

--- nvim/lua/configs/autocmds.lua
+++ nvim/lua/configs/autocmds.lua
@@ -21,3 +21,34 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		end
 	end,
 })
+
+-- NOTE: This is a workaround for
+local function augroup(name)
+	return vim.api.nvim_create_augroup("rafi_" .. name, {})
+end
+
+-- Resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	group = augroup("resize_splits"),
+	callback = function()
+		local current_tab = vim.fn.tabpagenr()
+		vim.cmd("tabdo wincmd =")
+		vim.cmd("tabnext " .. current_tab)
+	end,
+})
+
+-- Show cursor line only in active window
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+	group = augroup("auto_cursorline_show"),
+	callback = function(event)
+		if vim.bo[event.buf].buftype == "" then
+			vim.opt_local.cursorline = true
+		end
+	end,
+})
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+	group = augroup("auto_cursorline_hide"),
+	callback = function()
+		vim.opt_local.cursorline = false
+	end,
+})
