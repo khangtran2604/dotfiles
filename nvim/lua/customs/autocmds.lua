e73002167f7b1d0495ec21f4f69a10e93cab0ac8
Khang Tran <khangtran260495@gmail.com>
2024-06-08 15:16:18 +0700

    adding some extra autocmds

--- nvim/lua/customs/autocmds.lua
+++ nvim/lua/customs/autocmds.lua
@@ -1,5 +1,9 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 autocmd("VimEnter", {
   command = ":silent !kitty @ set-spacing padding=0 margin=0",
 })
@@ -11,7 +15,7 @@ autocmd("VimLeavePre", {
 -- Highlight on yank
 autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  group = augroup "highlight-on-yank",
   callback = function()
     vim.highlight.on_yank()
   end,
@@ -35,3 +39,46 @@ autocmd("BufReadPost", {
 
 -- No auto comment for next line
 autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- resize splits if window got resized
+autocmd({ "VimResized" }, {
+  group = augroup "resize_splits",
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd "tabdo wincmd ="
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+-- close some filetypes with <q>
+autocmd("FileType", {
+  group = augroup "close_with_q",
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+    "dbout",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- Fix conceallevel for json files
+autocmd({ "FileType" }, {
+  group = augroup "json_conceal",
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
