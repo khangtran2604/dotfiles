349343a9b7020709e627280a8f65396dc212d505
Khang Tran <khang.tm-ttv@tci-pf.com>
2024-06-21 08:17:34 +0700

    fix zenmode not work

--- nvim/lua/plugins/ui/zenmode.lua
+++ nvim/lua/plugins/ui/zenmode.lua
@@ -1,10 +1,8 @@
 return {
   'folke/zen-mode.nvim',
-  cmd = 'ZenMode',
   opts = {},
   keys = {
     {
-      'n',
       '<leader>z',
       ':ZenMode<CR>',
       { noremap = true, silent = true },
