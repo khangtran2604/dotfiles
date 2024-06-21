7f44dc5903e48fc793e55e1998fbcaccedfc0d20
Khang Tran <khangtran260495@gmail.com>
2024-06-21 20:26:30 +0700

    using neotree float instead

--- nvim/lua/plugins/navigate/neotree.lua
+++ nvim/lua/plugins/navigate/neotree.lua
@@ -1,17 +1,17 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-		cmd = "Neotree",
-		opts = {},
-		keys = function()
-			local map = vim.keymap.set
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  opts = {},
+  keys = function()
+    local map = vim.keymap.set
 
-			map('n', '<leader>e', '<CMD>Neotree<CR>', {desc = "Toggle Neotree", silent = true})
-			map('n', '<leader>o', '<CMD>Neotree focus<CR>', {desc = "Focus Neotree", silent = true})
-		end
+    map('n', '<leader>e', '<CMD>Neotree float<CR>', { desc = 'Toggle Neotree', silent = true })
+    -- map('n', '<leader>o', '<CMD>Neotree focus<CR>', { desc = 'Focus Neotree', silent = true })
+  end,
 }
