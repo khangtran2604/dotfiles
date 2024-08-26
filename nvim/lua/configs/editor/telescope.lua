80f802cd2433e6a248d793dc66071c897f2c38ae
Khang Tran <khang.tm-ttv@tci-pf.com>
2024-08-26 09:41:31 +0700

    update
    - Conform formatter for js,jsx,ts,tsx
    - Improve telescope performence

--- nvim/lua/configs/editor/telescope.lua
+++ nvim/lua/configs/editor/telescope.lua
@@ -9,7 +9,27 @@ return {
     local conf = require "nvchad.configs.telescope"
     local actions = require "telescope.actions"
 
-    conf.defaults.file_ignore_patterns = { "node_modules", "vendor" }
+    -- Using ripgrep
+    conf.defaults.vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    }
+    -- Reduce memory usage
+    conf.defaults.layout_strategy = "horizontal"
+    conf.defaults.layout_config = {
+      preview_cutoff = 120, -- Disable preview for large files
+    }
+    -- If you are working on a large project, you might want to split work across different jobs
+    conf.defaults.path_display = { "smart" }
+    conf.defaults.results_height = 10
+    conf.defaults.results_width = 8
+    -- Ignore some files
+    conf.defaults.file_ignore_patterns = { "node_modules", "vendor", ".git/" }
     conf.defaults.set_env = { COLORTERM = "truecolor" }
     conf.defaults.mappings = {
       i = {
@@ -32,6 +52,14 @@ return {
         ["<C-f>"] = actions.preview_scrolling_down,
       },
     }
+    -- Caching
+    conf.defaults.cache_picker = {
+      num_pickers = 20,
+    }
+    -- Enable Transparent Floats
+    conf.defaults.winblend = 10
+    -- Enable Debuggin
+    -- conf.defaults.debug = true
 
     conf.extensions.fzf = {
       fuzzy = true, -- false will only do exact matching
